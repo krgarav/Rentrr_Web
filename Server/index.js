1df4260dfd78fb74c37ada8409dbef5a670e1239
Kumar Gaurav <76562785+krgarav@users.noreply.github.com>
2025-04-16 17:38:57 +0530

    cors added

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://rentrr-web.onrender.com",
     credentials: true,
   })
 );
