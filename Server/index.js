ed6479ede4a71c9d1a0af2ff10c2f792b9a89d9e
Kumar Gaurav <76562785+krgarav@users.noreply.github.com>
2025-04-16 17:39:26 +0530

    cors added

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://rentrr-web.onrender.com",
+    origin: "https://rentrr.netlify.app",
     credentials: true,
   })
 );
