767d9f1d072c96a9b3de870d5f1dfade9b6d6a60
Kumar Gaurav <76562785+krgarav@users.noreply.github.com>
2025-04-17 10:20:00 +0530

    logs added

--- Server/controller/UserController.js
+++ Server/controller/UserController.js
@@ -77,8 +77,8 @@ exports.authenticateUser = async (req, res) => {
 exports.logoutUser = (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "Strict",
+    // secure: process.env.NODE_ENV === "production",
+    // sameSite: "Strict",
   });
 
   res.status(200).json({ message: "Logged out successfully" });
